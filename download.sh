63bf6d171946a7f989a9da05eb73b094b92c4858
subhashgunda <subhash.gunda@mckesson.com>
2018-07-13 16:10:18 -0400

    new changes

---
+++ download.sh
@@ -0,0 +1,3 @@
+date >> gsr.sh
+#wget http://s3.runnertech.com/clnaddr/clean_address_linux.zip -O clean_address_linux.zip
+#unzip -P CapitalOutside87 clean_address_linux.zip -d ./clean_address_linux
