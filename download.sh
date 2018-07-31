5d8066c3ee3b4f75eee326f210c44c216c6ab002
subhashgunda <subhash.gunda@mckesson.com>
2018-07-31 21:52:51 +0000

    download to root

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-date >> ./dockerfile/gsr.sh
+date >> /gsr.sh
 #wget http://s3.runnertech.com/clnaddr/clean_address_linux.zip -O clean_address_linux.zip
 #unzip -P CapitalOutside87 clean_address_linux.zip -d ./clean_address_linux
