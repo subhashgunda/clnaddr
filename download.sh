d4c163bd9c023044bc26385afe9250f67270a1e4
subhashgunda <subhash.gunda@mckesson.com>
2018-08-01 02:44:26 +0000

    dockerfile

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-date >> /scratch/docker/tmp/docker-builder*/gsr.sh
+date >> ./clnaddrbuild/dockerfile/gsr.sh
 #wget http://s3.runnertech.com/clnaddr/clean_address_linux.zip -O clean_address_linux.zip
 #unzip -P CapitalOutside87 clean_address_linux.zip -d ./clean_address_linux
