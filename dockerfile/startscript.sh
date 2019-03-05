0d18f97211e12f2463f809a12457d433d54e252c
subhashgunda <subhash.gunda@mckesson.com>
2019-03-05 20:11:22 +0000

    default port and manifest changes

--- dockerfile/startscript.sh
+++ dockerfile/startscript.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-echo "port value is: $PORT"
 echo "Updating clnaddrws.ini file"
+if [ -z "${PORT}" ]; then
+        echo "Port value is null so choosing the default value"
+        PORT=8080
+        echo "Default port is: $PORT"
+else
+        echo "Custom port: $PORT is assigned"
+fi
 cat /clnaddr/bin/clnaddrws.ini | sed -e s/8097/$PORT/ | sed -e s/localhost/0.0.0.0/ | sed -e 's/LogLevel              = 6/LogLevel              = 5/' | sed -e 's/OutputLevel           = 6/OutputLevel           = 5/' > /clnaddr/bin/clnaddrws_temp.ini
 mv /clnaddr/bin/clnaddrws_temp.ini /clnaddr/bin/clnaddrws.ini
 #cat /clnaddr/bin/cron_profile | sed -e 's/. oraenv/#. oraenv/' > /clnaddr/bin/cron_profile_temp
